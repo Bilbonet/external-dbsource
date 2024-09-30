d32e8b16be568af9f6dd3d445a8b9e4c6ad006fc
Jesus Ramiro <jesus@bilbonet.net>
2024-09-30 10:24:19 +0200

    Update __manifest__.py

--- base_external_import/__manifest__.py
+++ base_external_import/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Import data from external data sources.',
-    'version': '14.0.1.0.0',
+    'version': '15.0.1.0.0',
     'category': 'Tools',
     'author': "Jesus Ramiro, Liu Jianyun, Daniel Reis, "
               "Odoo Community Association (OCA)",
